af9fd3e6ea842e45e9375e59b4cbc41e2f44f3bd
SantosMaycon <mayconalves.santos14@gmail.com>
2020-04-23 12:25:09 -0300

    Adicionando banco de dados em memória para os testes de integração

--- src/test/java/com/maycon/minhasfinancas/repositories/UsuarioRepositoryTest.java
+++ src/test/java/com/maycon/minhasfinancas/repositories/UsuarioRepositoryTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.maycon.minhasfinancas.model.entities.Usuario;
 
 @SpringBootTest @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 	@Autowired
 	UsuarioRepository repository;
